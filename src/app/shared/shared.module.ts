e8bdad9ead3e32a3b80036fa8dcfd53c9d83c5ea
Jonathan Vieira <jonathansouza59@gmail.com>
2019-11-05 23:09:18 -0300

    Iniciando component data-form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,12 @@
+import { DropdownService } from './services/dropdown.service';
+import { InputFieldComponent } from './input-field/input-field.component';
+import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
+import { FormDebugComponent } from './form-debug/form-debug.componet';
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
+import { ErroMsgComponent } from './erro-msg/error-msg-component';
 
 @NgModule({
   imports: [
@@ -10,8 +15,20 @@ import { NgModule } from '@angular/core';
     FormsModule,
     ReactiveFormsModule
   ],
-  declarations: [],
-  exports: [],
-  providers: []
+  declarations: [
+    FormDebugComponent,
+    CampoControlErroComponent,
+    ErroMsgComponent,
+    InputFieldComponent
+  ],
+  exports: [
+    FormDebugComponent,
+    CampoControlErroComponent,
+    ErroMsgComponent,
+    InputFieldComponent
+  ],
+  providers: [
+    DropdownService
+  ]
 })
 export class SharedModule {}
