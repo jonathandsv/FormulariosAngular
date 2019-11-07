3a69d48b36664a0b771de6f87421cafc40d0cc16
Jonathan Vieira <jonathansouza59@gmail.com>
2019-11-06 23:10:53 -0300

    Iniciando codigo do component data-form

---
+++ src/app/data-form/data-form.component.ts
@@ -0,0 +1,141 @@
+import { ConsultaCepService } from './../shared/services/consulta-cep.service';
+import { DropdownService } from './../shared/services/dropdown.service';
+import { HttpClient } from '@angular/common/http';
+import { Cidade } from './../shared/models/cidade';
+import { BaseFormComponent } from '../shared/base-form/base-form.component';
+import { OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormValidations } from '../shared/form-validations';
+import { distinctUntilChanged, switchMap, tap, map } from 'rxjs/operators';
+import { empty } from 'rxjs';
+
+Component({
+  selector: 'app-data-form',
+  templateUrl: './data-form.component.html',
+  stylesUrls: ['./data-form.component.css']
+})
+export class DataFormComponent extends BaseFormComponent implements OnInit {
+
+  estados: EstadosBr[];
+  cidades: Cidade[];
+
+  cargos: any[];
+  tecnologias: any[];
+  newsletterOp: any[];
+
+  frameworks = ['Angular', 'React', 'Vue', 'Sencha'];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private dropdownService: DropdownService,
+    private cepService: ConsultaCepService,
+  ) {
+    super();
+  }
+
+  ngOnInit() {
+
+    this.dropdownService.getEstadosBr()
+    .subscribe(dados => this.estados = dados);
+
+    this.cargos = this.dropdownService.getCargos();
+    this.tecnologias = this.dropdownService.getTecnologias();
+    this.newsletterOp = this.dropdownService.getNewsletter();
+
+    this.formulario = this.formBuilder.group({
+      nome: [null, [ Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+      email: [null, [Validators.required, Validators.email], [this.validarEmail.bind(this)]],
+      confirmarEmail: [null, [FormValidations.equalsTo('email')]],
+
+      endereco: this.formBuilder.group({
+        cep: [null, [Validators.required, FormValidations.cepValidator]],
+        numero: [null, Validators.required],
+        complemento: [null],
+        rua: [null, Validators.required],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado: [null, Validators.required]
+      }),
+
+      cargo: [null],
+      tecnologias: [null];
+      newsletter: ['s'];
+      termos: [null, Validators.pattern('true')],
+      frameworks: this.buildFrameworks()
+    });
+
+    this.formulario.get('endereco.cep').statusChanges
+    .pipe(
+      distinctUntilChanged(),
+      tap(value => console.log('status CEP: ', value)),
+      switchMap(status => status === 'VALID' ?
+        this.cepService.consultaCEP(this.formulario.get('endereco.cep').value)
+        : empty()
+      )
+    )
+    .subscribe(dados => dados ? this.populaDadosForm(dados) : {});
+
+    this.formulario.get('endereco.estado').valueChanges
+    .pipe(
+      tap(estado => console.log('Novo estado: ', estado)),
+      map(estado => this.estados.filter(e => e.sigla === estado)),
+      map(estados => estados && estados.length > 0 ? estados[0].id : empty()),
+      switchMap((estadoId: number) => this.dropdownService.getCidades(estadoId)),
+      tap(console.log)
+    )
+    .subscribe(cidades => this.cidades = cidades);
+  }
+
+  buildFrameworks() {
+    const values = this.frameworks.map(v => new FormControl(false));
+
+    return this.formBuilder.array(values, FormValidations.requiredMinCheckbox(1));
+  }
+
+  submit() {
+    console.log(this.formulario);
+
+    let valueSubmit = Object.assign({}, this.formulario.value);
+
+    valueSubmit = Object.assign(valueSubmit, {
+      frameworks: valueSubmit.frameworks
+      .map((v, i) => v ? this.frameworks[i] : null)
+      .filter(v => v !== null)
+    });
+
+    console.log(valueSubmit);
+
+    this.http
+        .post('http://httpbin.org/post', JSON.stringify({}))
+        .subscribe(
+          dados => {
+            console.log(dados);
+          },
+          (error: any) => alert('erro')
+        );
+  }
+
+  consultCEP(){
+    const cep = this.formulario.get('endereco.cep').value;
+
+    if (cep != null && cep !== ''){
+      this.cepService.consultaCEP(cep)
+          .subscribe(dados => this.populaDadosForm(dados));
+    }
+  }
+
+  populaDadosForm(dados) {
+
+    this.formulario.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        complemento: dados.bairro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+}
